da1a15c03344062904d87b83aea3ea2b34f2892b
kimbaesung808 <kimbaesung808@gmail.com>
2025-05-07 03:33:17 -1000

    Add plan to create game

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hello World");
+/*
+Create the game "Rock, Paper, Scissors". The game will be played against the computer by the user. 
+Keep track of both the computer's and the user's scores. Write the logic to play one round and then five rounds.
+*/
