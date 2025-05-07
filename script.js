599daec947a626581527a5aa38622775bc8757ca
kimbaesung808 <kimbaesung808@gmail.com>
2025-05-07 09:05:23 -1000

    Add pseudocode

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 /*
 Create the game "Rock, Paper, Scissors". The game will be played against the computer by the user. 
 Keep track of both the computer's and the user's scores. Write the logic to play one round and then five rounds.
+
+CREATE function `getComputerChoice`
+  RETURN "rock", "paper", or "scissors", randomly
+
+CREATE function `getHumanChoice`
+  READ user input
+  RETURN user input
+
+CREATE variables `computerScore`, `humanScore` in global scope
+INIT `computerScore` and `humanScore` with 0
+
+WRITE logic to play one round
+CREATE function `playRound`
+  DEFINE parameters `computerChoice`, `humanChoice`
+  MAKE 'humanChoice' case-insensitive
+  DETERMINE winner
+  PRINT winner to console
+  INCREMENT winner's score
+
+WRITE logic to play five rounds
+EDIT 'playRound' or add helper function(s)
+CREATE function `playGame`
+  MOVE `playRound`, `computerScore`, `humanScore` declarations here
+  FOR five rounds
+    CALL `playRound`
+    INCREMENT winner's score
+  DETERMINE winner
+  PRINT winner to console
 */
